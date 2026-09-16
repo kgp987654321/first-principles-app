2d328f74a89f5ad7be6e64d33adcf33c489169af
kgp987654321 <kgpieffer@gmail.com>
2026-09-16 09:54:49 -0400

    Load expanded lesson game styling

--- src/expandedGameRouter.jsx
+++ src/expandedGameRouter.jsx
@@ -1,4 +1,5 @@
 import React from'react';
+import'./adventure4Games.css';
 import{ProbabilityBagGame,FairSpinnerGame,CombinationsGame,MatrixGame,AnalogyGame,LogicSwitchGame,ConstraintGame,TrialsGame,VectorGame,MomentumGame,TorqueGame,EnergyGame,GravityGame,OrbitGame,GraphChangeGame,AreaSlicesGame}from'./adventure4Games';
 const routes={
 'probability-bag':ProbabilityBagGame,'fair-spinner':FairSpinnerGame,combinations:CombinationsGame,matrix:MatrixGame,analogy:AnalogyGame,'logic-switch':LogicSwitchGame,constraint:ConstraintGame,trials:TrialsGame,vector:VectorGame,momentum:MomentumGame,torque:TorqueGame,energy:EnergyGame,gravity:GravityGame,orbit:OrbitGame,'graph-change':GraphChangeGame,'area-slices':AreaSlicesGame
