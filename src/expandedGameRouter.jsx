2b6e3d0c60c978dbe62802edbdd468490721a358
kgp987654321 <kgpieffer@gmail.com>
2026-09-16 09:51:11 -0400

    Route expanded lesson mechanics to new interactive games

---
+++ src/expandedGameRouter.jsx
@@ -0,0 +1,7 @@
+import React from'react';
+import{ProbabilityBagGame,FairSpinnerGame,CombinationsGame,MatrixGame,AnalogyGame,LogicSwitchGame,ConstraintGame,TrialsGame,VectorGame,MomentumGame,TorqueGame,EnergyGame,GravityGame,OrbitGame,GraphChangeGame,AreaSlicesGame}from'./adventure4Games';
+const routes={
+'probability-bag':ProbabilityBagGame,'fair-spinner':FairSpinnerGame,combinations:CombinationsGame,matrix:MatrixGame,analogy:AnalogyGame,'logic-switch':LogicSwitchGame,constraint:ConstraintGame,trials:TrialsGame,vector:VectorGame,momentum:MomentumGame,torque:TorqueGame,energy:EnergyGame,gravity:GravityGame,orbit:OrbitGame,'graph-change':GraphChangeGame,'area-slices':AreaSlicesGame
+};
+export function ExpandedGame({lesson,onSolved}){const Game=routes[lesson.mechanic];return Game?<Game lesson={lesson} onSolved={onSolved}/>:null}
+export function isExpandedMechanic(mechanic){return Boolean(routes[mechanic])}
